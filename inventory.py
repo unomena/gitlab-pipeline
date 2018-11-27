21e12dcd32204e0bcbe42c5210a7214a9ec78595
Shaun Sephton <shaun@sephton.io>
2018-11-27 18:35:48 +0200

    Start on dynamic deploy

---
+++ inventory.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import argparse
+import json
+import os
+import yaml
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--list', action='store_true')
+parser.add_argument('--host', action='store')
+args = parser.parse_args()
+
+
+if args.list:
+    print(json.dumps({
+        "all": {
+            "hosts": ['manager'],
+        },
+    }))
+
+elif args.host:
+    volumes = []
+    compose_yaml = yaml.load(open('docker-compose.yml', 'r').read())
+    for volume, spec in compose_yaml['volumes'].items():
+        if spec['external']:
+            volume = {
+                'name': spec['name']
+            }
+            for label in spec['labels']:
+                volume.update(label)
+            volumes.append(volume)
+
+    print(json.dumps({
+        'ansible_host': os.environ['QA_CLUSTER_IP'],
+        'volumes': volumes,
+        'ansible_ssh_extra_args': "-o StrictHostKeyChecking=no",
+        'ansible_python_interpreter': '/usr/bin/python3',
+        'ansible_ssh_private_key_file': 'keys/id_rsa',
+    }))
