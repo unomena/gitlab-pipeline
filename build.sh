bfa62c20391749f52d85935a3bbd797a08bfef8c
Shaun Sephton <shaun@sephton.io>
2018-12-05 09:21:07 +0200

    Add build helper script.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [ -e docker-compose-build.yml ]
+then
+    apk add --no-cache py-pip
+    pip install docker-compose
+    rm -f .env docker-compose.override.yml
+    docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
+    docker-compose --file docker-compose-build.yml build --pull
+    docker-compose --file docker-compose-build.yml push
+fi
