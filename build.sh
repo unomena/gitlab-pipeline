7552ed746df14dc08299783c0eb70c8f3c3f9e70
Shaun Sephton <shaun@sephton.io>
2018-12-12 10:50:57 +0200

    Experiment with image caching.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ then
     rm -f .env docker-compose.override.yml
     docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
     docker-compose --file docker-compose-build.yml pull
-    docker-compose --file docker-compose-build.yml build --pull
+    docker-compose --file docker-compose-build.yml build
     docker-compose --file docker-compose-build.yml push
 else
     echo "No docker-compose-build.yml file found, nothing to build."
