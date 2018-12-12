95efd4a6bc88bdea34a0847b0c60206febfb3f2c
Shaun Sephton <shaun@sephton.io>
2018-12-12 09:35:34 +0200

    Retry pre build pull.

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ then
     pip install docker-compose
     rm -f .env docker-compose.override.yml
     docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
+    docker-compose --file docker-compose-build.yml pull
     docker-compose --file docker-compose-build.yml build --pull
     docker-compose --file docker-compose-build.yml push
 else
