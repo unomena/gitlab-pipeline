c9589dd4c1dd832b7bdb0d3a9cd4ee3de27f1866
Shaun Sephton <shaun@sephton.io>
2018-12-12 11:30:49 +0200

    Pull cache_from images.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ then
     pip install docker-compose
     rm -f .env docker-compose.override.yml
     docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
-    docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -L1 docker pull
+    docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -I % docker pull %
     docker-compose --file docker-compose-build.yml build
     docker-compose --file docker-compose-build.yml push
 else
