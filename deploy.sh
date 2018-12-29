2eb76fa220b389cdc27431b058f07944d1bc3f03
Shaun Sephton <shaun@sephton.io>
2018-12-29 10:12:27 +0200

    Add deploy commands to shell script, set working dir perms to avoid ansile.cfg security warnings.

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#! /bin/sh -
+
+# Install required packages and ansible.
+apt-get -yy update && apt-get install -y openssh-client wget
+pip install ansible
+
+# Set working dir perms to avoud ansible.cfg security error, see
+# https://docs.ansible.com/ansible/devel/reference_appendices/config.html#cfg-in-world-writable-dir
+chmod 700 .
+
+# Add ssh key with which to execute ansible playbook.
+mkdir keys
+cp $CLUSTER_ADMIN_USER_SSH_PRIVATE_KEY_FILE keys/id_rsa
+chmod 700 keys/id_rsa
+
+# Fetch ansible playbook and config.
+wget https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/deploy.yml
+wget https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/ansible.cfg
+
+# Execute playbook.
+ansible-playbook -i $CLUSTER_IP, --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$QA_COMPOSE_FILE" deploy.yml
