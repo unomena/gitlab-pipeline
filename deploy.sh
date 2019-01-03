370f182992c1fadaa0e86c276c115da26cfe65dd
Shaun Sephton <shaun@sephton.io>
2019-01-03 11:09:36 +0200

    Avoid Host key verification failed errors.

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,12 @@ mkdir keys &&
 cp $CLUSTER_ADMIN_USER_SSH_PRIVATE_KEY_FILE keys/id_rsa &&
 chmod 700 keys/id_rsa &&
 
-# Fetch cluster host key
-ssh-keygen -R $CLUSTER_IP
-
 # Fetch ansible playbook and config.
 wget https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/deploy.yml &&
 wget https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/ansible.cfg &&
 
 # Fetch inventory from cluster
-scp -i keys/id_rsa admin@$CLUSTER_IP:/etc/ansible_inventory . &&
+scp -i keys/id_rsa -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory . &&
 
 # Execute playbook.
 ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" deploy.yml
