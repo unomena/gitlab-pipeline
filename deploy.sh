a71a34403701894adea11157b84e765c9fe18200
Shaun Sephton <shaun@sephton.io>
2019-01-03 10:56:51 +0200

    Use key.

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ wget https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/deploy.
 wget https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/ansible.cfg &&
 
 # Fetch inventory from cluster
-scp admin@$CLUSTER_IP:/etc/ansible_inventory . &&
+scp -i keys/id_rsa admin@$CLUSTER_IP:/etc/ansible_inventory . &&
 
 # Execute playbook.
 ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" deploy.yml
