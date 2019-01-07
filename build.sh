7ede1d30e8f19d3b4369b70be8f126bb4c39d7e9
Shaun Sephton <shaun@sephton.io>
2019-01-07 14:33:01 +0200

    Ensure .env vars are applied.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 if [ -e docker-compose-build.yml ]
 then
     rm -f .env docker-compose.override.yml
+    printenv > .env
     docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
     docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -I % docker pull %
     docker-compose --file docker-compose-build.yml build
