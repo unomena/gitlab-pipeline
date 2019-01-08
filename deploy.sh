446c6953209625278b4ed151110bddcffebbba23
Shaun Sephton <shaun@sephton.io>
2019-01-08 14:45:10 +0200

    Expirement with output

--- deploy.sh
+++ deploy.sh
@@ -27,4 +27,4 @@ envsubst < deploy.yml > playbook.yml
 # Execute playbook.
 ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" playbook.yml
 
-echo $STACK_HOSTNAME
+echo Stack deployed to https://$STACK_HOSTNAME
