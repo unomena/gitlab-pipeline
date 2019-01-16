60eed01c6c902578e85e8d38b62e40e9d1f0e8ce
Shaun Sephton <shaun@sephton.io>
2019-01-16 13:09:26 +0200

    Fetch ansible inventory from cluster from bastion host.

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,11 @@ envsubst < deploy.yml > playbook.yml
 rsync -avzhe "ssh -i ../keys/id_rsa -o StrictHostKeyChecking=No" . $BASTION_HOST_CONNECTION_STRING:~/$DEPLOY_PATH
 
 # Fetch Ansible inventory from cluster
-ssh -i ../keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING "scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory ~/$DEPLOY_PATH"
+ssh -i ../keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING << EOF
+    cd $DEPLOY_PATH
+    scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
+    ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" playbook.yml
+EOF
 
 echo Stack deployed to https://$STACK_HOSTNAME
 # Set working dir perms to avoid ansible.cfg security error, see
@@ -45,5 +49,4 @@ echo Stack deployed to https://$STACK_HOSTNAME
 
 
 # Execute playbook.
-#ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" playbook.yml
 
