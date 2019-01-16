30dd7344383e41da96a2d782206bef6f0feb21a6
Shaun Sephton <shaun@sephton.io>
2019-01-16 15:58:25 +0200

    Ensure destory workspace is always removed.

--- deploy.sh
+++ deploy.sh
@@ -41,4 +41,4 @@ ssh -i keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING <
     ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" playbook.yml
 EOF
 
-echo Stack deployed to https://$STACK_HOSTNAME
+echo Deployed stack to https://$STACK_HOSTNAME
--- destroy.sh
+++ destroy.sh
@@ -3,7 +3,18 @@
 # Exit on any failures
 set -e
 
-# Add ssh key with which to execute ansible playbook.
+# Generate random workspace path name
+WORKSPACE_NAME=workspace-$RANDOM-$RANDOM
+
+# Always cleanup bastion host workspace
+function cleanup {
+    ssh -i keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING << EOF
+    rm -rf $WORKSPACE_NAME
+EOF
+}
+trap cleanup EXIT
+
+# Add bastion host ssh key.
 mkdir keys
 cp /tmp/keys/GITLAB_USER_BASTION_HOST_SSH_PRIVATE_KEY keys/id_rsa
 chmod 700 keys/id_rsa
@@ -16,25 +27,15 @@ curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/ansi
 envsubst < destroy.yml > playbook.yml
 
 # Sync deploy artifacts to unique deploy path on bastion host.
-DEPLOY_PATH=deploy-$RANDOM-$RANDOM
-rsync -avzhe "ssh -i keys/id_rsa -o StrictHostKeyChecking=No" --exclude='.git' --exclude='keys' . $BASTION_HOST_CONNECTION_STRING:~/$DEPLOY_PATH
+rsync -avzhe "ssh -i keys/id_rsa -o StrictHostKeyChecking=No" --exclude='.git' --exclude='keys' . $BASTION_HOST_CONNECTION_STRING:~/$WORKSPACE_NAME
 
 # Fetch Ansible inventory from cluster and execute playbook on bastion host.
 ssh -i keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING << EOF
-    cd $DEPLOY_PATH
+    cd $WORKSPACE_NAME
     scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
     ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE" playbook.yml
     cd ..
-    rm -rf $DEPLOY_PATH
+    rm -rf $WORKSPACE_NAME
 EOF
 
-echo Stack serving https://$STACK_HOSTNAME destroyed.
-
-# Fetch inventory from cluster
-#scp -i keys/id_rsa -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
-
-# Set environment variables in playbook.
-#envsubst < destroy.yml > playbook.yml
-
-# Execute playbook.
-#ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE" playbook.yml
+echo Destroyed stack serving https://$STACK_HOSTNAME
