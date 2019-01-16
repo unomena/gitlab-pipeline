442ba918dd0486c33afbce8f0a3d2e7901bd4702
Shaun Sephton <shaun@sephton.io>
2019-01-16 15:28:09 +0200

    Execute destroy through bastion box.

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,9 @@ chmod 700 keys/id_rsa
 
 # Fetch ansible playbook, templates and config.
 mkdir -p templates
-curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/ansible.cfg -o ansible.cfg
 curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/deploy.yml -o deploy.yml
 curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/templates/env -o templates/env_unsub
+curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/ansible.cfg -o ansible.cfg
 
 # Replace environment variables in playbook.
 envsubst < deploy.yml > playbook.yml
@@ -29,8 +29,8 @@ ssh -i keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING <
     cd $DEPLOY_PATH
     scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
     ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" playbook.yml
+    cd ..
+    rm -rf $DEPLOY_PATH
 EOF
-
-ssh -i keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING "rm -rf DEPLOY_PATH"
 
 echo Stack deployed to https://$STACK_HOSTNAME
--- destroy.sh
+++ destroy.sh
@@ -3,24 +3,38 @@
 # Exit on any failures
 set -e
 
-# Set working dir perms to avoud ansible.cfg security error, see
-# https://docs.ansible.com/ansible/devel/reference_appendices/config.html#cfg-in-world-writable-dir
-chmod 700 .
-
 # Add ssh key with which to execute ansible playbook.
 mkdir keys
-cp $CLUSTER_ADMIN_USER_SSH_PRIVATE_KEY_FILE keys/id_rsa
+cp /tmp/keys/GITLAB_USER_BASTION_HOST_SSH_PRIVATE_KEY keys/id_rsa
 chmod 700 keys/id_rsa
 
 # Fetch ansible playbook and config.
 curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/destroy.yml -o destroy.yml
 curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/ansible.cfg -o ansible.cfg
 
-# Fetch inventory from cluster
-scp -i keys/id_rsa -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
-
-# Set environment variables in playbook.
+# Replace environment variables in playbook.
 envsubst < destroy.yml > playbook.yml
 
+# Sync deploy artifacts to unique deploy path on bastion host.
+DEPLOY_PATH=deploy-$RANDOM-$RANDOM
+rsync -avzhe "ssh -i keys/id_rsa -o StrictHostKeyChecking=No" --exclude='.git' --exclude='keys' . $BASTION_HOST_CONNECTION_STRING:~/$DEPLOY_PATH
+
+# Fetch Ansible inventory from cluster and execute playbook on bastion host.
+ssh -i keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING << EOF
+    cd $DEPLOY_PATH
+    scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
+    ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE" playbook.yml
+    cd ..
+    rm -rf $DEPLOY_PATH
+EOF
+
+echo Stack serving https://$STACK_HOSTNAME destroyed.
+
+# Fetch inventory from cluster
+#scp -i keys/id_rsa -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
+
+# Set environment variables in playbook.
+#envsubst < destroy.yml > playbook.yml
+
 # Execute playbook.
-ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE" playbook.yml
+#ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE" playbook.yml
