2732b9e2639b5185c7229abedb42e0937b03f8c1
Shaun Sephton <shaun@sephton.io>
2019-01-18 12:34:31 +0200

    Remove post build pattern. All build steps should occur within the docker build phase itself.

--- build.sh
+++ build.sh
@@ -9,10 +9,6 @@ then
     docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
     docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -I % docker pull % || true
     docker-compose --file docker-compose-build.yml build
-    if [ -e docker/commands/post_build.sh ]
-    then
-        ./docker/commands/post_build.sh
-    fi
     docker-compose --file docker-compose-build.yml push
 else
     echo "No docker-compose-build.yml file found, nothing to build."
