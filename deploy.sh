99cb6fff6c9337b9ad48a04a6e8a0dd2b6586658
Shaun Sephton <shaun@sephton.io>
2019-01-22 16:18:26 +0200

    Limit rsync to only files required

--- deploy.sh
+++ deploy.sh
@@ -44,7 +44,7 @@ rsync -avzhe "ssh -i keys/id_rsa -o StrictHostKeyChecking=No" payload $BASTION_H
 ssh -i keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING << EOF
     cd $WORKSPACE_NAME
     scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
-    ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" playbook.yml
+    ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" deploy.yml
 EOF
 
 echo Deployed stack to https://$STACK_HOSTNAME
