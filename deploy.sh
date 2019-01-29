81aa3b941a4a6fa49d4efa2b10595753627de922
Shaun Sephton <shaun@sephton.io>
2019-01-29 16:13:33 +0200

    Experiment with ssh agent reverse forwarding.

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,8 @@ trap cleanup EXIT
 
 # Add bastion host ssh key.
 
+#apt install openssh-client -y
+eval $(ssh-agent -s)
 echo "$GITLAB_USER_BASTION_HOST_SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 
@@ -23,8 +25,6 @@ mkdir keys
 cp /tmp/keys/GITLAB_USER_BASTION_HOST_SSH_PRIVATE_KEY keys/id_rsa
 chmod 700 keys/id_rsa
 
-apt install openssh-client -y
-eval $(ssh-agent -s)
 ssh -i keys/id_rsa -n -o 'ForwardAgent yes' -o 'StrictHostKeyChecking=No' $BASTION_HOST_CONNECTION_STRING 'ssh-add'
 echo "DEBUG"
 
