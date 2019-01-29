232b32c6c505a8583fefd9296876824105b9332a
Shaun Sephton <shaun@sephton.io>
2019-01-29 16:45:22 +0200

    Experiment with ssh agent reverse forwarding.

--- destroy.sh
+++ destroy.sh
@@ -30,17 +30,4 @@ scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
 export ANSIBLE_FORCE_COLOR=1
 ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE" playbook.yml
 
-
-# Sync deploy artifacts to unique deploy path on bastion host.
-#rsync -avzhe "ssh -i keys/id_rsa -o StrictHostKeyChecking=No" --exclude='.git' --exclude='keys' . $BASTION_HOST_CONNECTION_STRING:~/$WORKSPACE_NAME
-
-# Fetch Ansible inventory from cluster and execute playbook on bastion host.
-#ssh -i keys/id_rsa -o StrictHostKeyChecking=No $BASTION_HOST_CONNECTION_STRING << EOF
-#    cd $WORKSPACE_NAME
-#    scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory .
-#    ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE" playbook.yml
-#    cd ..
-#    rm -rf $WORKSPACE_NAME
-#EOF
-
 echo Destroyed stack serving https://$STACK_HOSTNAME
