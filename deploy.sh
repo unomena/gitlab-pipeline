b18e462808d387ae70a4ce6a35c83ba0c52c8423
Shaun Sephton <shaun@sephton.io>
2019-01-29 15:49:47 +0200

    Experiment with ssh agent reverse forwarding.

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ mkdir keys
 cp /tmp/keys/GITLAB_USER_BASTION_HOST_SSH_PRIVATE_KEY keys/id_rsa
 chmod 700 keys/id_rsa
 
-ssh -o 'ForwardAgent yes' $BASTION_HOST_CONNECTION_STRING 'ssh-add'
+ssh -o 'ForwardAgent yes' -o 'StrictHostKeyChecking=No' $BASTION_HOST_CONNECTION_STRING 'ssh-add'
 ssh-add -l
 
 # Fetch ansible playbook, templates and config.
