d6e3bb26d4dca85c5e4eb0970ea1a73cd2f9bed8
Shaun Sephton <shaun@sephton.io>
2019-01-29 16:18:15 +0200

    Experiment with ssh agent reverse forwarding.

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ trap cleanup EXIT
 
 #apt install openssh-client -y
 eval $(ssh-agent -s)
-echo "$GITLAB_USER_BASTION_HOST_SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+echo "$GITLAB_USER_BASTION_HOST_SSH_PRIVATE_KEY" | ssh-add - > /dev/null
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
