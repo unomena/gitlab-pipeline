3ac458946ec8d2b6497f6730512febffc64c4dce
Shaun Sephton <shaun@sephton.io>
2019-06-10 16:59:42 +0200

    Experiment with transcrypt decryption.

--- build.sh
+++ build.sh
@@ -3,15 +3,9 @@
 # Exit on any failures
 set -e
 
-if [ -e .blackbox ]
-then
-    gpg --batch --import /tmp/keys/GITLAB_BLACKBOX_GPG_KEY
-    #gpg --list-keys
-    blackbox_postdeploy
-fi
-
 if [ -e docker-compose-build.yml ]
 then
+    transcrypt --yes --cipher=aes-256-cbc --password='$TRANSCRIPT_PASSWORD'
     rm -f .env docker-compose.override.yml
     docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
     docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -I % docker pull % || true
