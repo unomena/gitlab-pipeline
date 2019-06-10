2330178e8c22863127d16019c51663ef54bee312
Shaun Sephton <shaun@sephton.io>
2019-06-10 19:41:00 +0200

    Only transcrypt if a password is set.

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 # Exit on any failures
 set -e
 
+# Apply transcrypt credentials on repo to decrypt encrypted files.
+if [[ -z "${TRANSCRYPT_PASSWORDA}" ]]; then
+    transcrypt --yes --cipher=aes-256-cbc --password="$TRANSCRYPT_PASSWORD"
+fi
+
 if [ -e docker-compose-build.yml ]
 then
-    transcrypt --yes --cipher=aes-256-cbc --password="$TRANSCRYPT_PASSWORD"
-    cat src/project/settings.py
     rm -f .env docker-compose.override.yml
     docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
     docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -I % docker pull % || true
