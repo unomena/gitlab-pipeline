0e2c4c1324d5876e7ecf8bc54bf6b9054a5a22fd
Shaun Sephton <shaun@sephton.io>
2019-06-10 15:01:41 +0200

    Only execute blackbox decryption when .blackbox path exists.

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@
 # Exit on any failures
 set -e
 
+if [ -e .blackbox ]
+then
+    gpg --batch --import /tmp/keys/GITLAB_BLACKBOX_GPG_KEY
+    blackbox_postdeploy
+fi
+
 if [ -e docker-compose-build.yml ]
 then
-    #gpg --batch --import /tmp/keys/GITLAB_BLACKBOX_GPG_KEY
-    #blackbox_postdeploy
     rm -f .env docker-compose.override.yml
     docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
     docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -I % docker pull % || true
