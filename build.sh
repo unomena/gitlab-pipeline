89be5c0eb848f3173e48f4f64f4ec5efb6d64e08
Shaun Sephton <shaun@sephton.io>
2019-07-09 08:36:16 +0200

    Experiment with test script checking that only latest tags are built when a pipeline is triggered instead of manually executed.

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@
 # Exit on any failures
 set -e
 
+if [ "$CI_JOB_TRIGGERED" == "true" ]
+then
+    LATEST_TAG="$(git describe --abbrev=0 --tags)"
+    if [ "$CI_COMMIT_TAG" != "$LATEST_TAG" ]
+    then
+        exit 1
+    fi
+fi
+
 # Apply transcrypt credentials on repo to decrypt encrypted files.
 if [ -z "$TRANSCRYPT_PASSWORD" ]
 then
