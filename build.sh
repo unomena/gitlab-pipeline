711866fe3f73e5b3afd8796a2784ebf14f6261cd
Shaun Sephton <shaun@sephton.io>
2019-07-09 08:55:03 +0200

    Experiment with test script checking that only latest tags are built when a pipeline is triggered instead of manually executed.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -e
 
 
-LATEST_TAG=`git ls-remote --tags --quiet | tail -1 | awk '{split($0,a,"/"); print a[3]'`
+LATEST_TAG=`git ls-remote --tags --quiet | tail -1 | awk '{split($0,a,"/"); print a[3]}'`
 
 
 echo $LATEST_TAG
