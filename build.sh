c282f0645316464c495e45b0ac3599ccb864b45c
Shaun Sephton <shaun@sephton.io>
2019-07-23 11:55:48 +0200

    Revert "Use GitLab registry variables if set to authenticate to registry instead of special gitlab-ci-token user."
    This reverts commit 8d359560524b52903db34d614ac222d680f37344.

--- build.sh
+++ build.sh
@@ -14,12 +14,7 @@ fi
 if [ -e docker-compose-build.yml ]
 then
     rm -f .env docker-compose.override.yml
-    if [ -z "$REGISTRY_USERNAME" ]
-    then
-        docker login --username $REGISTRY_USERNAME --password $REGISTRY_PASSWORD $CI_REGISTRY
-    else
-        docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
-    fi
+    docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
     docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -I % docker pull % || true
     docker-compose --file docker-compose-build.yml build
     docker-compose --file docker-compose-build.yml push
