8d359560524b52903db34d614ac222d680f37344
Shaun Sephton <shaun@sephton.io>
2019-07-23 10:21:05 +0200

    Use GitLab registry variables if set to authenticate to registry instead of special gitlab-ci-token user.

--- build.sh
+++ build.sh
@@ -14,7 +14,12 @@ fi
 if [ -e docker-compose-build.yml ]
 then
     rm -f .env docker-compose.override.yml
-    docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
+    if [ -z "$REGISTRY_USERNAME" ]
+    then
+        docker login --username $REGISTRY_USERNAME --password $REGISTRY_PASSWORD $CI_REGISTRY
+    else
+        docker login --username gitlab-ci-token --password $CI_JOB_TOKEN $CI_REGISTRY
+    fi
     docker-compose --file docker-compose-build.yml config | python -c "import sys, yaml; print('\n'.join(['\n'.join(service['build'].get('cache_from', [])) for service in yaml.load(sys.stdin)['services'].values()]))" | xargs -I % docker pull % || true
     docker-compose --file docker-compose-build.yml build
     docker-compose --file docker-compose-build.yml push
