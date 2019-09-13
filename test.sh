0e45b1afbd76c7017b5c0f23bdccdf19228572ab
Shaun Sephton <shaun@sephton.io>
2019-09-13 15:52:40 +0200

    Fold deploy into test.

--- test.sh
+++ test.sh
@@ -20,10 +20,18 @@ ssh -n -o 'ForwardAgent yes' -o 'StrictHostKeyChecking=No' $BASTION_HOST_CONNECT
 mkdir -p templates
 mkdir -p payload/templates
 curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/test.yml -o test.yml
+curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/templates/env -o templates/env
+curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/templates/docker-compose.override.yml -o payload/templates/docker-compose.override.yml
 curl -s https://gitlab.unomena.net/unomenapublic/gitlab-pipeline/raw/master/ansible.cfg -o ansible.cfg
 
 # Replace environment variables in playbook.
-envsubst < test.yml > payload/test.yml
+envsubst < deploy.yml > payload/test.yml
+
+# Replace environment variables in env template file.
+envsubst < templates/env > payload/templates/env
+
+# Add test compose file to payload.
+cp docker-compose-test.yml payload/
 
 # Fetch Ansible inventory from cluster
 scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory payload/
@@ -32,4 +40,4 @@ scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory payload
 cd payload
 export ANSIBLE_FORCE_COLOR=1
 export ANSIBLE_PIPELINING=1
-ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" test.yml
+ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=docker-compose-test.yml" test.yml
