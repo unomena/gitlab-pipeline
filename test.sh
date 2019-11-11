eb93aa4dbc41cbda5832473e5548f6bf1b9c09a3
Shaun Sephton <shaun@sephton.io>
2019-11-11 09:33:24 +0200

    Test Stage: Revert to using explicit test compose file instead of complicating matters with an overrides structure

--- test.sh
+++ test.sh
@@ -21,7 +21,7 @@ mkdir -p templates
 mkdir -p payload/templates
 curl -s $PIPELINE_ASSETS_ROOT_URL/test.yml -o test.yml
 curl -s $PIPELINE_ASSETS_ROOT_URL/templates/env_test -o templates/env
-curl -s $PIPELINE_ASSETS_ROOT_URL/templates/docker-compose.override.yml -o payload/templates/docker-compose.deploy.yml
+curl -s $PIPELINE_ASSETS_ROOT_URL/templates/docker-compose.override.yml -o payload/templates/docker-compose.override.yml
 curl -s $PIPELINE_ASSETS_ROOT_URL/ansible.cfg -o ansible.cfg
 
 # Replace environment variables in playbook.
@@ -30,11 +30,8 @@ envsubst < test.yml > payload/test.yml
 # Replace environment variables in env template file.
 envsubst < templates/env > payload/templates/env
 
-# Add test compose file to payload.
-#cp docker-compose-test.yml payload/
 # Add indicated compose file to payload.
-cp docker-compose.yml payload/
-cp docker-compose.override.yml payload/
+cp $COMPOSE_FILE payload/
 
 # Fetch Ansible inventory from cluster
 scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory payload/
@@ -43,4 +40,4 @@ scp -o StrictHostKeyChecking=No admin@$CLUSTER_IP:/etc/ansible_inventory payload
 cd payload
 export ANSIBLE_FORCE_COLOR=1
 export ANSIBLE_PIPELINING=1
-ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY" test.yml
+ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" test.yml
