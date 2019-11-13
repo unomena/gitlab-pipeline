00ae9aa9f1c3e9ca35ebdfac54f099eeaef44192
Shaun Sephton <shaun@sephton.io>
2019-11-13 08:25:57 +0200

    Experiment with nosetesrts artifacts.

--- deploy.sh
+++ deploy.sh
@@ -47,6 +47,7 @@ export ANSIBLE_FORCE_COLOR=1
 export ANSIBLE_PIPELINING=1
 ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" deploy.yml
 
+ls -lh
 pwd
 more nosetests.xml
 cd ..
