efc39058263599d16368bf983f2233b13af30075
Shaun Sephton <shaun@sephton.io>
2019-11-13 11:20:50 +0200

    Debug test report.

--- deploy.sh
+++ deploy.sh
@@ -47,11 +47,6 @@ export ANSIBLE_FORCE_COLOR=1
 export ANSIBLE_PIPELINING=1
 ansible-playbook -i ansible_inventory --extra-vars "ansible_sudo_pass=$CLUSTER_ADMIN_USER_PASSWORD ci_job_token=$CI_JOB_TOKEN ci_registry=$CI_REGISTRY resource_prefix=$RESOURCE_PREFIX stack_hostname=$STACK_HOSTNAME stage=$STAGE aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY compose_file=$COMPOSE_FILE" deploy.yml
 
-# Move test report to build path to ensure GitLab can source it as an artifact.
-cd ..
-cp payload/test_report.html .
-more test_report.html
-
 echo Deployed stack to https://$STACK_HOSTNAME
 
 if [ $STAGE = "staging" ] || [ $STAGE = "production" ]; then
